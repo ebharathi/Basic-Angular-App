1c89539ab9d8f690db39caca94d6d2bdcbf5104a
ebharathi <elayabharathi10102002@gmail.com>
2023-10-11 00:59:14 +0530

    Added Navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-
+import { HomeComponent } from './home/home.component';
 @Component({
   selector: 'app-root',
   // templateUrl: './app.component.html',
   // styleUrls: ['./app.component.css'],
-  template:`<h1>Hello World!</h1>`
+  template:`
+    <app-navbar></app-navbar>
+`
 })
 export class AppComponent {
   title = 'homes';
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-home',
+  template: `
+    <p>
+      home works!
+    </p>
+  `,
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+}
